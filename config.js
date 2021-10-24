b8a4bda9664928d478c23978f17e8f93c36a19af
Matthew Nordstrand <matt_nordstrand@live.com>
2021-10-24 12:06:17 -0400

    Completed W4 Workshop

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': "123123-23423-345-3456456",
-    'mongoUrl': "mongodb://localhost:27017/nucampsite"
+    'mongoUrl': "mongodb://localhost:27017/nucampsite",
+    'facebook': {
+        clientId: 'YOUR FACEBOOK APP ID HERE', //I don't have a facebook, so I haven't done this part yet.
+        clientSecret: 'YOUR FACEBOOK APP SECRET HERE'
+    }
 }
